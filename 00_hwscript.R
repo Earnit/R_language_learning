f5e19abb36f5d6500b0b60f1df9816e8b6119a29
Anton Golovin <golovin.reserve@gmail.com>
2012-11-27 21:54:46 +0300

    first steps

--- 00_hwscript.R
+++ 00_hwscript.R
@@ -3,4 +3,23 @@ print("HelloWorld")
 
 # Generate random number with point 14 digits after ,
 # 2-size of array; 1,10-range
-x <- runif(2,1,10)
+x <- runif(1,1,10)
+print(x)
+
+# Generate whole number
+# 1:10-range; 10-size of array; replace-repeats are allowed
+x <- sample(1:10, 10, replace = TRUE)
+print(x)
+
+
+#Experiment with real think
+price_delta <- sample(-50:50, 100, replace = TRUE)
+price <- 1000
+
+# outputbuf <- vector()
+outputbuf <- price
+for (delta in price_delta) {
+  outputbuf <- c(outputbuf,  tail(outputbuf, n=1)+delta)
+}
+
+plot(outputbuf, type="l")
