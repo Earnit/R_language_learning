071ecd9ec6423a1e5b8f61d9907fc6c66b359038
Anton <thisis@mymail.com>
2012-12-04 16:42:37 +0400

    Add coments, continue datainput part

--- 02_datainput/01_datainput_datatypes.R
+++ 02_datainput/01_datainput_datatypes.R
@@ -1,18 +1,59 @@
-a <- c(1,2,5.3,6,-2,4) # numeric vector
+# I took this code from http://www.statmethods.net/input/datatypes.html
 
-b <- c("one","two","three") # character vector
 
-c <- c(TRUE,TRUE,TRUE,FALSE,TRUE,FALSE) #logical vector
+##
+# VECTORS
+##
 
-outbuf = a[c(2,4)] # 2nd and 4th elements of vector
-print(outbuf)
+print("Numeric vector")
+print(a <- c(1,2,5.3,6,-2,4))
 
-# generates 5 x 4 numeric matrix 
-y<-matrix(1:20, nrow=5,ncol=4)
+# Character vector
+b <- c("one","two","three") 
 
-# another example
+# Logical vector
+c <- c(TRUE,TRUE,TRUE,FALSE,TRUE,FALSE) 
+
+print("2nd and 4th elements of vector")
+print(a[c(2,4)])
+
+
+##
+# MATRICES
+##
+
+# Generates 5 x 4 numeric matrix 
+x<-matrix(1:20, nrow=5,ncol=4)
+
+# Another example, 
 cells <- c(1,26,24,68)
 rnames <- c("R1", "R2")
 cnames <- c("C1", "C2") 
-mymatrix <- matrix(cells, nrow=2, ncol=2, byrow=TRUE,
+
+# Dimnames is names for dimensions of matrix, 
+#  rnames could be changed by c("asd", "qwe") for example
+mymatrix <- matrix(cells, nrow=2, ncol=2, byrow=TRUE, 
                    dimnames=list(rnames, cnames))
+
+print("Our matrix")
+print(x)
+
+print("4th column of matrix")
+print(x[,4]) 
+
+print("3rd row of matrix")
+print(x[3,])
+
+print("rows 2,3,4 of columns 1,2,3")
+print(x[2:4,1:3])
+
+
+##
+# DATA FRAMES
+##
+
+d <- c(1,2,3,4)
+e <- c("red", "white", "red", NA)
+f <- c(TRUE,TRUE,TRUE,FALSE)
+mydata <- data.frame(d,e,f)
+names(mydata) <- c("ID","Color","Passed") # variable names 
