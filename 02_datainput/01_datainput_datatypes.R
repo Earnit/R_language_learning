58c60e5f6734167e2e7ee69acc198b314dd40dae
Anton Golovin <golovin.reserve@gmail.com>
2012-12-05 23:48:32 +0300

    Data frames finished

--- 02_datainput/01_datainput_datatypes.R
+++ 02_datainput/01_datainput_datatypes.R
@@ -21,6 +21,7 @@ z <- a[c(2,4)]
 ##
 # MATRICES
 ##
+print("### MATRICES ###")
 
 # Generates 5 x 4 numeric matrix 
 x<-matrix(1:20, nrow=5,ncol=4)
@@ -51,13 +52,33 @@ print(x[2:4,1:3])
 ##
 # DATA FRAMES
 ##
-
-d <- c(1,2,3,4)
-e <- c("red", "white", "red", NA)
-f <- c(TRUE,TRUE,TRUE,FALSE)
+print("### DATA FRAMES ###")
+  
+id <- c(1,2,3,4)
+col <- c("red", "white", "red", NA)
+bol <- c(TRUE,TRUE,TRUE,FALSE)
+vol <- c(3, 5, 7, 11)
+age <- c(10, 53, 19, 22)
 
 # Creates data frames, used as the fundamental data structure
-mydata <- data.frame(d,e,f)
+mydata <- data.frame(id,col,bol,vol, age)
 
 # Set the variable names of the object
-names(mydata) <- c("ID","Color","Passed") 
+names(mydata) <- c("ID","Color","Passed", "Volume", "Age") 
+print(mydata)
+
+# Сolumns 3,4,5 of data frame
+print(mydata[3:5])
+
+# Columns ID and Age from data frame
+print(mydata[c("ID","Age")])
+
+# Variable Age in the data frame 
+print(mydata$Age)
+
+
+##
+# List
+##
+print("### LIST ###")
+
